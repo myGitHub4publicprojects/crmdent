9f01affc67647fa9e1d2ab18a19f6cdb33d395bf
myGitHub4publicprojects <jszczep@gmail.com>
2019-10-09 07:09:42 +0200

    Add models

---
+++ kadent/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+from django.core.urlresolvers import reverse
+
+
+from .validators import accepted_extensions, accepted_size
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    notes = models.TextField(null=True, blank=True)
+
+    def full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def get_absolute_url(self):
+        return reverse('kadent:patient_edit', kwargs={'pk': self.pk})
+
+    def str(self):
+        return self.full_name()
+
+class Visit(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(settings.AUTH_USER_MODEL)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('kadent:visit_detail', kwargs={'pk': self.pk})
+
+
+
+class Image(models.Model):
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(
+        upload_to='documents/', validators=[accepted_extensions, accepted_size])
+    note = models.TextField(null=True, blank=True)
+    uploaded_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    visit = models.ForeignKey(
+        Patient, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+    def get_absolute_url(self):
+        return reverse('kadent:image_detail', kwargs={'pk': self.pk})
+
---
+++ kadent/validators.py
@@ -0,0 +1,6 @@
+def accepted_extensions(file):
+    pass
+
+
+def accepted_size(file):
+    pass
