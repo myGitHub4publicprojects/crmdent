da27e8257a191e39456a7bc0d68701378e27246d
myGitHub4publicprojects <jszczep@gmail.com>
2019-11-03 06:51:50 +0100

    Add comments to validators

--- kadent/validators.py
+++ kadent/validators.py
@@ -1,13 +1,14 @@
 from django.core.exceptions import ValidationError
 from django.conf import settings
 
+# unused in this version
 def accepted_size(file):
     if file.size < settings.MIN_UPLOAD_SIZE:
         raise ValidationError('This seems to be empty file. Use a file with size over 1kb')
     if file.size > settings.MAX_UPLOAD_SIZE:
         raise ValidationError('This file is too large. Use a file with size under 20MB')
 
-
+# unused in this version
 def accepted_extensions():
     pass
 
