2b7b58447c996a61b2cfb68a72691b7aff2a5ae1
myGitHub4publicprojects <jszczep@gmail.com>
2019-11-13 17:20:51 +0100

    Add buttons to return to patient view

--- kadent/views.py
+++ kadent/views.py
@@ -63,6 +63,10 @@ class VisitUpdate(LoginRequiredMixin, UpdateView):
     def get_initial(self):
         return {'note': self.object.note}
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['patient_id'] = self.object.patient.id
+        return context
 
 class VisitDelete(LoginRequiredMixin, DeleteView):
     model = Visit
