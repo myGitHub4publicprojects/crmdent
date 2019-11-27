2ec1aced423fa8c07ffcf9360c7dd7494b36c324
myGitHub4publicprojects <jszczep@gmail.com>
2019-11-27 09:32:03 +0100

    Remove secret key from settings

--- crmdent/settings.py
+++ crmdent/settings.py
@@ -1,22 +1,11 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l@@uchw+@wm@(-ckeyt=t1rnnw=@xice+ew__-$by+w2%ky8rj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
